c3bc0e7acd2720ffe716bfdb2d2e72c571d937af
Gildo Farias <gildofarias3@gmail.com>
2024-06-22 12:47:24 -0300

    Mapeamento EntidadeDadosPadroes

--- src/main/java/com/comunicacao/domain/EntidadeDadosPadroes.java
+++ src/main/java/com/comunicacao/domain/EntidadeDadosPadroes.java
@@ -1,5 +1,42 @@
 package com.comunicacao.domain;
 
+import com.comunicacao.utils.EntidadeUtils;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Data
+@Embeddable
 public class EntidadeDadosPadroes {
+
+    @Setter(AccessLevel.NONE)
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_envio")
+    private LocalDateTime dataEnvio;
+
+    @Column(name = "tentativas", columnDefinition = EntidadeUtils.INT_DEFAULT_0)
+    private int tentativas;
+
+    @Column(name = "enviado", columnDefinition = EntidadeUtils.BOOLEAN_DEFAULT_FALSE)
+    private boolean enviado;
+
+    @Column(name = "falha", columnDefinition = EntidadeUtils.BOOLEAN_DEFAULT_FALSE)
+    private boolean falha;
+
+    @Column(name = "log_falha", columnDefinition = EntidadeUtils.TEXT)
+    private String logFalha;
+
+    @PrePersist
+    public void prePersist() {
+        this.dataCriacao = LocalDateTime.now();
+    }
 
 }
--- src/main/java/com/comunicacao/utils/EntidadeUtils.java
+++ src/main/java/com/comunicacao/utils/EntidadeUtils.java
@@ -3,4 +3,8 @@ package com.comunicacao.utils;
 public class EntidadeUtils {
 
     public static final String BIGSERIAL = "BIGSERIAL";
+    public static final String BOOLEAN_DEFAULT_FALSE = "boolean DEFAULT false";
+    public static final String TEXT = "TEXT";
+    public static final String INT_DEFAULT_0 = "int DEFAULT 0";
+
 }
