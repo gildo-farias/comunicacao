d9602279fa63cde81711280db1b96f3a8bbf035c
Gildo Farias <gildofarias3@gmail.com>
2024-06-22 10:28:25 -0300

    Initial commit

---
+++ src/main/java/com/comunicacao/domain/EnvioDeComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.comunicacao.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioDeComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioDeComunicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comunicacao/domain/EnvioDeComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comunicacao.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioDeComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
