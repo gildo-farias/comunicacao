8164af5f92d897d646374a42441b0be25f3cca51
Gildo Farias <gildofarias3@gmail.com>
2024-06-22 23:08:49 -0300

    Criando Controller Exception Handler

---
+++ src/main/java/com/comunicacao/configuracao/controllers/ControllerExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.comunicacao.configuracao.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RequiredArgsConstructor
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
