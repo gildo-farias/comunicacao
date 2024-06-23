1887b3fa36bbc3e5974662a13c1f99e0488ca1ae
Gildo Farias <gildofarias3@gmail.com>
2024-06-23 20:44:07 -0300

    Teste Entidade: Melhorias com beforeAll

--- src/test/java/com/comunicacao/entidades/AgendamentoComunicacaoEntidadeTest.java
+++ src/test/java/com/comunicacao/entidades/AgendamentoComunicacaoEntidadeTest.java
@@ -1,21 +1,23 @@
 package com.comunicacao.entidades;
 
-import com.comunicacao.controllers.mappers.AgendamentoComunicaoEntidadeMapper;
 import com.comunicacao.domain.agendamento.AgendamentoStatus;
 import com.comunicacao.services.MocksMappers;
 import com.comunicacao.services.TesteAnotacaoMockito;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class AgendamentoComunicacaoEntidadeTest extends TesteAnotacaoMockito {
 
-    private AgendamentoComunicacaoEntidade montarEntidade() {
-        return AgendamentoComunicaoEntidadeMapper.mapear(MocksMappers.mapearRequisicaoCompleta());
+    private static AgendamentoComunicacaoEntidade entidade;
+
+    @BeforeAll
+    static void iniciar() {
+        entidade = MocksMappers.mapearEntidade();
     }
 
     @Test
     void agendamentoProcessando() {
-        AgendamentoComunicacaoEntidade entidade = this.montarEntidade();
         entidade.processando();
         Assertions.assertThat(entidade.getStatus()).isEqualTo(AgendamentoStatus.PROCESSANDO);
         Assertions.assertThat(entidade.getTentativas()).isEqualTo(1);
@@ -23,14 +25,12 @@ public class AgendamentoComunicacaoEntidadeTest extends TesteAnotacaoMockito {
 
     @Test
     void agendamentoEnviado() {
-        AgendamentoComunicacaoEntidade entidade = this.montarEntidade();
         entidade.enviado();
         Assertions.assertThat(entidade.getStatus()).isEqualTo(AgendamentoStatus.ENVIADO);
     }
 
     @Test
     void agendamentoDeuFalha() {
-        AgendamentoComunicacaoEntidade entidade = this.montarEntidade();
         String causa = "Servidor fora do ar!";
         entidade.falha(causa);
         Assertions.assertThat(entidade.getStatus()).isEqualTo(AgendamentoStatus.FALHA);
