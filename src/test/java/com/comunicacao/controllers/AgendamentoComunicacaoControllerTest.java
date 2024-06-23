d8a0ed4a9e42a7401b6aaeb05160d30dbc3c1d79
Gildo Farias <gildofarias3@gmail.com>
2024-06-23 17:24:05 -0300

    Teste controller: apagar

---
+++ src/test/java/com/comunicacao/controllers/AgendamentoComunicacaoControllerTest.java
@@ -0,0 +1,31 @@
+package com.comunicacao.controllers;
+
+import com.comunicacao.services.AgendamentoComunicacaoService;
+import com.comunicacao.services.TesteAnotacaoMockito;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class AgendamentoComunicacaoControllerTest extends TesteAnotacaoMockito {
+
+    @InjectMocks
+    private AgendamentoComunicacaoController controller;
+
+    @Mock
+    private AgendamentoComunicacaoService service;
+
+    @Test
+    public void givenCodigoRastreioWhenDeletarAgendamentoThenDeveRetornar204() {
+        String codigoRastreio = "5b421cf7-0ed2-47a9-916e-21c167b76173";
+
+        ResponseEntity<Void> response = controller.apagar(codigoRastreio);
+
+        Mockito.verify(service).apagar(codigoRastreio);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/test/java/com/comunicacao/services/TesteAnotacaoMockito.java
@@ -0,0 +1,9 @@
+package com.comunicacao.services;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class TesteAnotacaoMockito {
+
+}
