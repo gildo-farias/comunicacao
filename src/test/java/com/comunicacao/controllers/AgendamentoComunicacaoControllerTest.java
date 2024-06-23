69924ea488b77e54ee756ca3986b7a65f44e0575
Gildo Farias <gildofarias3@gmail.com>
2024-06-23 20:04:52 -0300

    Teste Agendamento Controller: visualizar status retorna 200

--- src/test/java/com/comunicacao/controllers/AgendamentoComunicacaoControllerTest.java
+++ src/test/java/com/comunicacao/controllers/AgendamentoComunicacaoControllerTest.java
@@ -3,6 +3,7 @@ package com.comunicacao.controllers;
 import com.comunicacao.controllers.requisicoes.AgendamentoComunicacaoRequisicao;
 import com.comunicacao.controllers.respostas.RespostaCodigoRastreio;
 import com.comunicacao.controllers.respostas.RespostaStatus;
+import com.comunicacao.domain.agendamento.AgendamentoStatus;
 import com.comunicacao.services.AgendamentoComunicacaoService;
 import com.comunicacao.services.MocksMappers;
 import com.comunicacao.services.TesteAnotacaoMockito;
@@ -14,6 +15,8 @@ import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Optional;
+
 public class AgendamentoComunicacaoControllerTest extends TesteAnotacaoMockito {
 
     @InjectMocks
@@ -24,7 +27,7 @@ public class AgendamentoComunicacaoControllerTest extends TesteAnotacaoMockito {
 
     @Test
     public void givenCodigoRastreioWhenDeletarAgendamentoThenDeveRetornar204() {
-        String codigoRastreio = "5b421cf7-0ed2-47a9-916e-21c167b76173";
+        String codigoRastreio = MocksMappers.CODIGO_RASTREIO;
         ResponseEntity<Void> response = controller.apagar(codigoRastreio);
 
         Mockito.verify(service).apagar(codigoRastreio);
@@ -33,14 +36,27 @@ public class AgendamentoComunicacaoControllerTest extends TesteAnotacaoMockito {
 
     @Test
     public void givenCodigoRastreioWhenVisualizarStatusAgendamentoThenDeveRetornar404() {
-        String codigoRastreio = "5b421cf7-0ed2-47a9-916e-21c167b76173";
+        String codigoRastreio = MocksMappers.CODIGO_RASTREIO;
         ResponseEntity<RespostaStatus> response = controller.visualizarStatus(codigoRastreio);
 
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 
     @Test
-    public void givenDadosAgendamentoWhenAgendarAgendamentoThenDeveRetornar202() {
+    public void givenCodigoRastreioWhenVisualizarStatusAgendamentoThenDeveRetornar200() {
+        String codigoRastreio = MocksMappers.CODIGO_RASTREIO;
+        AgendamentoStatus aguardando = AgendamentoStatus.AGUARDANDO;
+        Mockito.when(service.visualizarStatus(codigoRastreio)).thenReturn(Optional.of(aguardando));
+
+        RespostaStatus respostaStatus = new RespostaStatus(aguardando.name());
+        ResponseEntity<RespostaStatus> response = controller.visualizarStatus(codigoRastreio);
+
+        Assertions.assertThat(response.getBody()).isEqualTo(respostaStatus);
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+    @Test
+    public void givenDadosAgendamentoWhenCriarAgendamentoThenDeveRetornar202() {
         AgendamentoComunicacaoRequisicao requisicao = MocksMappers.mapearRequisicaoCompleta();
         ResponseEntity<RespostaCodigoRastreio> response = controller.agendar(requisicao);
 
