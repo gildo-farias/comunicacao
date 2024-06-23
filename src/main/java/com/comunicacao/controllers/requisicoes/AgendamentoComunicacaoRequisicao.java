e0e6738842856d2aebf1826c6f9bf06fbfc156e8
Gildo Farias <gildofarias3@gmail.com>
2024-06-23 20:37:36 -0300

    melhorando Validacao da requisicao

--- src/main/java/com/comunicacao/controllers/requisicoes/AgendamentoComunicacaoRequisicao.java
+++ src/main/java/com/comunicacao/controllers/requisicoes/AgendamentoComunicacaoRequisicao.java
@@ -8,10 +8,18 @@ import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDateTime;
 
 public record AgendamentoComunicacaoRequisicao(
-        @NotNull(message = "data hora de envio inválida") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraEnvio,
-        @NotBlank(message = "contéudo inválido") String conteudo,
-        @NotBlank(message = "destino inválido") String destino,
-        @NotBlank(message = "tipo inválido") @Pattern(regexp = "EMAIL|SMS|PUSH|WHATSAPP") String tipo
+        @NotNull(message = DATA_HORA_DE_ENVIO_INVALIDA) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataHoraEnvio,
+        @NotBlank(message = CONTEUDO_INVALIDO) String conteudo,
+        @NotBlank(message = DESTINO_INVALIDO) String destino,
+        @NotBlank(message = TIPO_NAO_PODE_SER_VAZIO) @Pattern(regexp = TIPO_DISPONIVEIS, message = TIPO_INVALIDO) String tipo
 ) {
+
+    static final String DATA_HORA_DE_ENVIO_INVALIDA = "data hora de envio inválida";
+    static final String CONTEUDO_INVALIDO = "contéudo inválido";
+    static final String DESTINO_INVALIDO = "destino inválido";
+    static final String TIPO_NAO_PODE_SER_VAZIO = "tipo inválido";
+
+    private static final String TIPO_DISPONIVEIS = "EMAIL|SMS|PUSH|WHATSAPP";
+    static final String TIPO_INVALIDO = "tipo inválido, tipos disponíveis: " + TIPO_DISPONIVEIS;
 
 }
