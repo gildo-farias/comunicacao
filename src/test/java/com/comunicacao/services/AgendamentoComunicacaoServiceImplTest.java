3557a6e553313a65902ab4dc27a4da82ba1b179e
Gildo Farias <gildofarias3@gmail.com>
2024-06-23 19:53:14 -0300

    Teste Agendamento Service: criar, apagar e visualizar

--- src/test/java/com/comunicacao/services/AgendamentoComunicacaoServiceImplTest.java
+++ src/test/java/com/comunicacao/services/AgendamentoComunicacaoServiceImplTest.java
@@ -1,12 +1,12 @@
 package com.comunicacao.services;
 
-import com.comunicacao.controllers.mappers.AgendamentoComunicaoEntidadeMapper;
 import com.comunicacao.entidades.AgendamentoComunicacaoEntidade;
 import com.comunicacao.repositories.AgendamentoComunicacaoRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 
 public class AgendamentoComunicacaoServiceImplTest extends TesteAnotacaoMockito {
 
@@ -17,10 +17,27 @@ public class AgendamentoComunicacaoServiceImplTest extends TesteAnotacaoMockito 
     private AgendamentoComunicacaoRepository repository;
 
     @Test
-    public void agendarSucesso() {
-        AgendamentoComunicacaoEntidade entidade = AgendamentoComunicaoEntidadeMapper.mapear(MocksMappers.mapearRequisicaoCompleta());
+    public void criarAgendamento() {
+        AgendamentoComunicacaoEntidade entidade = MocksMappers.mapearEntidade();
+        Mockito.when(repository.saveAndFlush(entidade)).thenReturn(entidade);
         String codigoRastreio = service.agendar(entidade);
         Assertions.assertThat(codigoRastreio).isNotBlank();
+    }
+
+    @Test
+    public void apagarAgendamento() {
+        AgendamentoComunicacaoEntidade entidade = MocksMappers.mapearEntidade();
+        String codigoRastreio = entidade.getCodigoRastreio();
+        service.apagar(codigoRastreio);
+        Mockito.verify(repository).apagarPeloCodigoDeRastreio(codigoRastreio);
+    }
+
+    @Test
+    public void visualizarAgendamento() {
+        AgendamentoComunicacaoEntidade entidade = MocksMappers.mapearEntidade();
+        String codigoRastreio = entidade.getCodigoRastreio();
+        service.visualizarStatus(codigoRastreio);
+        Mockito.verify(repository).buscarStatusPeloCodigoRastreio(codigoRastreio);
     }
 
 }
